8bf7a3e83a04a101f09aa2fb583e6a5b220f2d8f
Jeremy Woods <jeremywoods124@gmail.com>
2022-11-13 08:38:19 -0500

    Convert templats from static data to dynamic JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const food = JSON.parse(fs.readFileSync('./data/food.json', 'utf8'));
+
 const meals = (req, res) => {
-    res.render('meals', {title: 'Travlr Getways'});
+    pageTitle = process.env.npm_package_description + ' - Meals';
+    res.render('meals', {title: pageTitle, food});
 }
 
 module.exports = {
