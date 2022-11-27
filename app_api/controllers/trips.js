6bfec1d6200bfa9d09a8400c51d4f69a9fe40732
Jeremy Woods <jeremywoods124@gmail.com>
2022-11-27 09:48:50 -0500

    Create new application API

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
 const model = mongoose.model('trips');
 
-// GET: /trips      lists all the trips
+// GET: /trips - lists all trips
 const tripsList = async (req, res) => {
     model
-        .find({}) // empty filter for all
+        .find({})  
         .exec((err, trips) => {
             if (!trips) {
                 return res
                     .status(404)
-                    .json({ "message": "trip not found" });
+                    .json({ "message": "Trip not found" });
             } else if (err) {
-                return res 
+                return res
                     .status(404)
                     .json(err);
             } else {
                 return res
                     .status(200)
-                    .json(trips)
+                    .json(trips);
             }
         });
 };
 
-// GET: /trips/:tripCode     returns a single trip
+// GET: /trips/:tripCode - returns a single trip
 const tripsFindCode = async (req, res) => {
     model
-        .find({ 'code': req.params.tripCode}) // empty filter for all
+        .find({ 'code': req.params.tripCode })
         .exec((err, trip) => {
             if (!trip) {
                 return res
                     .status(404)
-                    .json({ "message": "trip not found" });
+                    .json({ "message": "Trip not found" });
             } else if (err) {
-                return res 
+                return res
                     .status(404)
                     .json(err);
             } else {
